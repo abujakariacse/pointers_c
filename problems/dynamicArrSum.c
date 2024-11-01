39f3c4acb347b8c0a2b131f0af9933029a686fa4
Abu Jakaria <abujakaria316@gmail.com>
2024-11-02 01:39:27 +0600

    dynamic array sum

---
+++ problems/dynamicArrSum.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    long long sum = 0;
+
+    int *arr = (int *)malloc(100000 * sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    free(arr);
+
+    long long result = llabs(sum);
+
+    printf("%lld", result);
+
+    return 0;
+}
